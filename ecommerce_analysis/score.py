ad5038887a516f03fa568ec47a8d3b06271da129
Pablo Diaz <pedj18@gmail.com>
2023-05-19 17:43:56 +0000

    Add post method to obtain predictions

--- ecommerce_analysis/score.py
+++ ecommerce_analysis/score.py
@@ -1,37 +1,45 @@
+#!/usr/bin/env python
+# encoding: utf-8
 # código para ejecutar inferencia
 import joblib
+import json
+from flask import Flask, jsonify, request
+import pandas as pd
+import logging
 
-# Agregar features
 class Score():
+    '''clase utilizada para predecir en batch o dejar api disponible'''
     def calculate_score_df(self, pdf, model):
+        '''calcular predicciones de pandas dataframe'''
         return model.predict(pdf)
 
+    def get_score_batch(self):
+        '''calcular predicciones batch'''
+        # rutas lectura datasets
+        path_to_store_datasets = "/workspaces/ecommerce_analysis/data/stage=preprocess/"
+
+        dict_data = joblib.load(path_to_store_datasets + 'train_test_split.joblib')
+
+        X_test = dict_data['X_test']
+
+        scorer = Score()
+        X_test['pred'] = scorer.calculate_score_df(X_test, model)
+        logging.info('predictions stored')
+
+# rutas lectura modelos
 path_to_store_models = "/workspaces/ecommerce_analysis/models/"
-path_to_store_datasets = "/workspaces/ecommerce_analysis/data/stage=preprocess/"
+
+# api
+app = Flask(__name__)
 
 # best model
 model = joblib.load(path_to_store_models + 'best_model.joblib')
-dict_data = joblib.load(path_to_store_datasets + 'train_test_split.joblib')
-
-X_train = dict_data['X_train']
-X_test = dict_data['X_test']
-Y_train = dict_data['Y_train']
-Y_test = dict_data['Y_test']
-
-scorer = Score()
-X_test['pred'] = scorer.calculate_score_df(X_test, model)
-
-#!/usr/bin/env python
-# encoding: utf-8
-import json
-from flask import Flask, jsonify, request
-
-app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
-def score_record():
+def get_score_api():
+    '''metodo post para obtener prediccion'''
     record = json.loads(request.data)
-    record['paso'] = 'si'
+    record['predict'] = model.predict(pd.DataFrame([record]))[0]
     return jsonify(record)
 
 app.run()
--- ecommerce_analysis/test_pytest.py
+++ ecommerce_analysis/test_pytest.py
@@ -5,3 +5,29 @@ def test_unicity_search_parquet():
     df = pd.read_parquet("/workspaces/ecommerce_analysis/data/stage=raw/source=search/dataformat=parquet")
     id_col = 'id'
     assert len(df) == len(df[id_col].unique())
+
+test_case_api_score = {
+    "order_backend": 25,
+    "price": 5690.4,
+    "discount_percentage": 0.0,
+    "available_quantity": 1,
+    "listing_type_id_bronze": 0,
+    "listing_type_id_free": 0,
+    "listing_type_id_gold_pro": 1,
+    "listing_type_id_gold_special": 0,
+    "buying_mode_buy_it_now": 1,
+    "site_id_MLA": 0,
+    "site_id_MLB": 1,
+    "site_id_MLC": 0,
+    "site_id_MPE": 0,
+    "category_id_MLA1002": 0,
+    "category_id_MLB1002": 1,
+    "category_id_MLC1002": 0,
+    "category_id_MPE1002": 0,
+    "has_discount_0": 1,
+    "has_discount_1": 0,
+    "accepts_mercadopago_True": 1,
+    "condition_new": 1,
+    "condition_not_specified": 0,
+    "condition_used": 0
+}
